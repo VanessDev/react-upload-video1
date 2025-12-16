efa67964d30557bdd63f38bc859b28b8f245b23b
LorianaD <loriana.diano@gmail.com>
2025-12-16 16:18:28 +0100

    home

--- front/src/components/Pages/Home.jsx
+++ front/src/components/Pages/Home.jsx
@@ -1,8 +1,53 @@
+import { useEffect, useState } from "react";
+
 function Home() {
+
+    const [videos, setVideos] = useState([]);
+    const [error, setError] = useState("");
+
+    async function videoList() {
+
+        console.log("fonction videolist ok");
+
+        try {
+            console.log("try ok");
+            
+            const response = await fetch("http://localhost:3000/api/video");
+
+            if (response.ok) {
+
+                const data = await response.json();
+                console.log(data);
+                
+                setVideos(data.videos);
+
+            } else {
+
+                setError("Erreur lors du chargement des vidéos");
+
+            }
+
+        } catch (err) {
+
+            setError("Erreur serveur");
+
+        }
+    }
+
+    useEffect(() => {
+        videoList();
+    }, [])
+
     return(
-        <>
-            <h2>Home</h2>
-        </>
+        <div>
+            <h2>Bienvenue sur VIADEO !</h2>
+            <div>
+               {error && <p>{error}</p>} 
+            </div>
+            <div>
+                
+            </div>
+        </div>
     )
 }
 
