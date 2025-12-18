eafb71e70be1a77e003f872db6d63a89e0ce5bb7
LorianaD <loriana.diano@gmail.com>
2025-12-18 11:18:29 +0100

    add search

--- front/src/components/Pages/Stream.jsx
+++ front/src/components/Pages/Stream.jsx
@@ -42,7 +42,7 @@ function Stream() {
     return (
         <div className="page gap-[24px]">
             <h2 className="text-primary font-bold text-4xl">{video.title}</h2>
-            <video controls width="1000" src={`http://localhost:3000/api/video/${video.id}/stream`}></video>
+            <video controls width="1000" src={`http://localhost:3000/api/video/${video.id}/stream`} className="h-[80vh]"></video>
             <p>{video.theme}</p>
             <p>{video.description}</p>
             <div className="rating">
