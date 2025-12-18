2ec575ee4408bebdaf04e75e5a68ab9b2c5ddf2e
LorianaD <loriana.diano@gmail.com>
2025-12-18 09:51:19 +0100

    test commit

--- front/src/components/Pages/Stream.jsx
+++ front/src/components/Pages/Stream.jsx
@@ -40,9 +40,18 @@ function Stream() {
     }
 
     return (
-        <div className="page">
-            <h2>{video.title}</h2>
+        <div className="page gap-[24px]">
+            <h2 className="text-primary font-bold text-4xl">{video.title}</h2>
             <video controls width="1000" src={`http://localhost:3000/api/video/${video.id}/stream`}></video>
+            <p>{video.theme}</p>
+            <p>{video.description}</p>
+            <div className="rating">
+                <div className="mask mask-star bg-primary" aria-label="1 star"></div>
+                <div className="mask mask-star bg-primary" aria-label="2 star"></div>
+                <div className="mask mask-star bg-primary" aria-label="3 star" aria-current="true"></div>
+                <div className="mask mask-star bg-primary" aria-label="4 star"></div>
+                <div className="mask mask-star bg-primary" aria-label="5 star"></div>
+            </div>
         </div>
     )
 }
