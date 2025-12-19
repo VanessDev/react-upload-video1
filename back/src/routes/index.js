d3393df5abad7118e16b633c21d00558989853dc
VanessDev <vanessa.biamonti@gmail.com>
2025-12-19 12:48:23 +0100

    videos8

--- back/src/routes/index.js
+++ back/src/routes/index.js
@@ -5,13 +5,12 @@ import themeRoutes from "./themeRoutes.js";
 
 const router = Router();
 
-// routes publiques
+
 router.use("/video", videoRoutes);
 
-// si tes commentRoutes contiennent déjà /comments dedans, garde "/"
 router.use("/", commentRoutes);
 
-// ✅ IMPORTANT : /theme (pas ./theme)
+
 router.use("/theme", themeRoutes);
 
 export default router;
